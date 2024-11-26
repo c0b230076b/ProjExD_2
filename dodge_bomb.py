f41a7f60b5db6a432ea92437b2ac83dbb4c33877
Kotaro <c0b230076b@edu.teu.ac.jp>
2024-11-26 13:43:37 +0900

    サイズ変更

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -12,8 +13,13 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
+    bb_img.set_colorkey((0,0,0))
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    # bb_rct = bb_img.get_rect()
+    # bb_rct.center = random.randrange(WIDTH), random.randrange(HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -21,9 +27,10 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        # screen.blit(bb_img, [random.randrange(WIDTH), random.randrange(HEIGHT)])
+        # screen.blit(bb_img, bb_rct)
 
-
-        DELTA = {pg.K_UP:(0,-5), pg.K_DOWN:(0,+5), pg.K_LEFT:(-5,0), pg.K_RIGHT:(+5,0)}
+        # DELTA = {pg.K_UP:(0,-5), pg.K_DOWN:(0,+5), pg.K_LEFT:(-5,0), pg.K_RIGHT:(+5,0)}
 
 
         key_lst = pg.key.get_pressed()
@@ -38,6 +45,8 @@ def main():
             sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        # bb_rct.move_ip(vx, vy)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
